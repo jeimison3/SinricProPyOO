2aacb2d351986e672efc93f36601bb8156614431
Jeimison Moreno <jeimison.ti@gmail.com>
2020-05-25 11:59:39 -0300

    Cleaning main file

--- main.py
+++ main.py
@@ -5,30 +5,6 @@ from credentials import appKey,secretKey
 from credentials import thing_1
 
 
-def onSetBrightness(did, state):
-    # Alexa, ligar Device a VALUE%
-    print(did, 'BrightnessLevel : ', state)
-    return True, state
-
-def onAdjustBrightness(did, state):
-    # Alexa increase/decrease device brightness by 44
-    print(did, 'AdjustBrightnessLevel : ', state)
-    return True, state
-    
-def onSetPowerLevel(did, powerLevel):
-    # Alexa set power level device by 44
-    print(did, 'SetPowerLevel : ', powerLevel)
-    return True,50
-
-
-
-callbacks = {
-'powerState': onPowerState,
-'setBrightness': onSetBrightness,
-'adjustBrightness': onAdjustBrightness,
-'setPowerLevel' : onSetPowerLevel,
-}
-
 ''' 
 Implemented functions for dimming light
 '''
